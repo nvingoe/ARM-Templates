3c55e0bb4e3cc0fd06ac25d4e7389adc4bd783b3
Neil Vingoe <neil.vingoe@fdbhealth.com>
2020-03-04 23:05:09 +0000

    structural changes

--- Demo/API/RunTheDeploy.ps1
+++ Demo/API/RunTheDeploy.ps1
@@ -1,15 +1,21 @@
 
+
 $mytenant = "hearstpm.onmicrosoft.com"
 $mysubscription = "Visual Studio Professional"
-$resourceGroupName = "Neils_Demo_RG"
+$resourceGroupName = "neilsBrownBag"
 $resourceGroupLocation = "uksouth"
-$templatename = "deploy.json"
-$parameterFileName = "deploy.parameters.local.json"
+$templateFilename = "./deploy.json"
+$parameterFileName = "./deploy.parameters.local.json"
 
 Set-AzContext -Tenant $mytenant  -SubscriptionId $mysubscription 
-New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force 
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatename -TemplateParameterFile $parameterFileName
+az account set -s $mysubscription
+New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilename   -TemplateParameterFile $parameterFileName -Mode Complete
+
+
+
+
 
 
 
--- Demo/RunTheDeploy.ps1
+++ Demo/RunTheDeploy.ps1
@@ -3,13 +3,18 @@ $mytenant = "hearstpm.onmicrosoft.com"
 $mysubscription = "Visual Studio Professional"
 $resourceGroupName = "Neils_Demo_RG"
 $resourceGroupLocation = "uksouth"
-$templatename = ".SQL\deploy.json"
-$parameterFileName = ".SQL\azuredeploy.parameters.local.json"
+
+$SQLTemplateName = "SQL\deploy.json"
+$SQLParameterFileName = "SQL\deploy.parameters.local.json"
+$appTemplateName = "API\deploy.json"
+$appParameterFileName = "API\deploy.parameters.local.json"
 
 Set-AzContext -Tenant $mytenant  -SubscriptionId $mysubscription 
+
 New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force 
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatename -TemplateParameterFile $parameterFileName
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $SQLTemplateName -TemplateParameterFile $SQLparameterFileName
 
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $appTemplateName -TemplateParameterFile $appParameterFileName
 
 
