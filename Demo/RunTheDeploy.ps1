003cd113b5be5d41ce8f7b75be8fb948e4541dec
Neil Vingoe <neil.vingoe@fdbhealth.com>
2020-03-06 17:13:20 +0000

    -

--- Demo/RunTheDeploy.ps1
+++ Demo/RunTheDeploy.ps1
@@ -1,19 +1,29 @@
 
-$mytenant = "hearstpm.onmicrosoft.com"
-$mysubscription = "Visual Studio Professional"
-$resourceGroupName = "Neils_Demo_RG"
+$tenantName = "hearstpm.onmicrosoft.com"
+$subscriptionName = "Visual Studio Professional"
+$resourceGroupName = "neilsBrownBagRGG"
 $resourceGroupLocation = "uksouth"
 $SQLTemplateName = "SQL\deploy.json"
 $SQLParameterFileName = "SQL\deploy.parameters.local.json"
 $appTemplateName = "API\deploy.json"
 $appParameterFileName = "API\deploy.parameters.local.json"
 
-Set-AzContext -Tenant $mytenant  -SubscriptionId $mysubscription 
 
-New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force 
+Set-AzContext -Tenant $tenantName  -SubscriptionId $subscriptionName 
+
+az group exists --subscription $subscriptionName  --name $resourceGroupName
+
+
+if ((az group exists --subscription $subscriptionName  --name $resourceGroupName) -eq "true") {
+  
+    Remove-AzResourceGroup -Name $resourceGroupName
+}
+else {
+
+    new-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation 
+}
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $SQLTemplateName -TemplateParameterFile $SQLparameterFileName
-
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $appTemplateName -TemplateParameterFile $appParameterFileName
 
 
